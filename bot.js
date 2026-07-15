2c88017e2c19b24ee5cd4d3b862e00b54770edf7
KK-gn <kjw970819@gmail.com>
2026-07-15 21:50:57 +0900

    Delete bot.js

--- bot.js
+++
@@ -1,5 +0,0 @@
-const { AuthClient, ChatClient } = require('node-kakao');
-
-console.log("====================================");
-console.log("카카오톡 봇 서버가 성공적으로 시작되었습니다!");
-console.log("====================================");

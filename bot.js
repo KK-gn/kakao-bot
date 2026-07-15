9111653dafd6381d9a1253ae9080536ee29dfb53
KK-gn <kjw970819@gmail.com>
2026-07-15 10:03:08 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const { AuthClient, ChatClient } = require('node-kakao');
 
-// 여기에 카카오톡 로그인 및 봇 구동 코드가 들어갑니다.
-console.log("카카오톡 봇 서버 구동 시작");
+console.log("====================================");
+console.log("카카오톡 봇 서버가 성공적으로 시작되었습니다!");
+console.log("====================================");
